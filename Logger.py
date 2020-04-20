c3983d79fa71d4b5410c31f501e2ea03cb201484
Anna <annettlebid@gmail.com>
2020-04-20 13:52:47 +0300

    Ex3

--- Logger.py
+++ Logger.py
@@ -7,8 +7,12 @@ class Logger:
     def add_to_log(msg):
         timestamp = datetime.now()
         timestamp_str = timestamp.strftime("%d-%b-%Y %H:%M:%S-")
-        f = open("D:\\Anna\\Bootcamp exercises\\python\\Python-mini-project\\log.txt", "a")
-        f.write(timestamp_str + " " +msg + "\n")
+        try:
+            f = open(os.path.join(location, 'logger.txt'));
+            f = open("D:\\Anna\\Bootcamp exercises\\python\\Python-mini-project\\log.txt", "a")
+            f.write(timestamp_str + " " +msg + "\n")
+        except Exception as e:
+            print("error logging {}, error is {}".format(msg, e.args[0]))
 
 
 
---
+++ User.py
@@ -0,0 +1,18 @@
+import csv
+import os
+from collections import OrderedDict
+
+
+class User:
+    def user_auth(name):
+        with open('D:\\Anna\\Bootcamp exercises\\python\\Python-mini-project\\User.csv') as users_file:
+            users_file_reader = csv.reader(users_file)
+            for row in users_file_reader:
+                if row[1] == name:
+                    print(row)
+                    return (row[6])
+                else:
+                    return False
+
+
+print(User.user_auth("Anna"))
