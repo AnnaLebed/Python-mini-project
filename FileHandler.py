65841bc7ce92de2ce5ce19c0bf5312b8bc1d380c
Anna <annettlebid@gmail.com>
2020-04-23 06:58:03 +0300

    Day 2 Ex2

--- FileHandler.py
+++ FileHandler.py
@@ -34,7 +34,6 @@ class FileHandler:
 
     def write_to_file(self, data):
         try:
-
             print(self.readen_data[0])
             columns = self.readen_data.pop(0)
             if len(data) != len(self.readen_data[0]):
@@ -76,52 +75,38 @@ class FileHandler:
             print("Error: " + str(e))
             return False
 
-    # def update_csv(self, file_name, id, data):
-    #     try:
-    #        self.load_from_csv(file_name)
-    #
-    #
-    #         columns =  self.readen_data.pop(0)
-    #
-    #
-    #         for index, row in enumerate( self.readen_data, start=0):
-    #             if int(row[0]) == int(id):
-    #
-    #                 self.readen_data.pop(index)
-    #
-    #
-    #                 self.readen_data.insert(index, data)
-    #                 break
-    #
-    #         # returning columns names back to the content (at the first place)
-    #         self.readen_data.insert(0, columns)
-    #
-    #         # storing content back to the file
-    #
-    #         заменить
-    #         на
-    #         write
-    #         to
-    #         file:
-    #
-    #         self.write_to_file(data)
-    #
-    #         writer = csv.writer(open(self.get_file_path(file_name), 'w', newline=''))
-    #
-    #         writer.writerows(content)
-    #
-    #         return True
-    #     except Exception as e:
-    #         print("Error: " + str(e))
-    #         return False
+    def update_csv(self, file_name, id, data):
+        try:
+            loaded_data = self.load_from_csv(file_name)
+            columns = loaded_data.pop(0)
 
-# fh = FileHandler()
-# # fh.load_from_csv('User.csv')
-#
+            for num, row in enumerate(loaded_data, start=0):
+                if int(row[0]) == int(id):
+
+                    loaded_data.pop(num)
+                    loaded_data.insert(num, data)
+                    break
+
+            # returning columns names back to the content
+            loaded_data.insert(0, columns)
+            # storing content back to the file
+            writer = csv.writer(open(self.file_path, 'w', newline=''))
+
+            writer.writerows(loaded_data)
+
+            return True
+        except Exception as e:
+            print("Error: " + str(e))
+            return False
+
+fh = FileHandler()
+fh.update_csv('User.csv', 91111, [911111, 'updated first', 'updated last', 'password', 'manager', '80000', 'chef'])
+
+# # fh.load_from_csv('User.csv')#
 #
 # print(fh.append_to_csv('User.csv', [1001000, 'Daniel', 'Leffiz', '101010', 'manager', 20000, 'employee']))
-
-# print(fh.remove_from_csv('user.csv', 888888))
+#
+# print(fh.remove_from_csv('user.csv', 777777))
 #
 #
 #
